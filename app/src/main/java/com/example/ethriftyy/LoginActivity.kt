018970e6b0f219c95d1d196f3627846a939dd717
SharonWendoh <swendoh.sharon@gmail.com>
2022-11-08 06:35:54 +0300

    added login and welcome page

--- app/src/main/java/com/example/ethriftyy/LoginActivity.kt
+++ app/src/main/java/com/example/ethriftyy/LoginActivity.kt
@@ -1,20 +1,11 @@
 package com.example.ethriftyy
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
-
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
+        setContentView(R.layout.activity_login)
     }
 }
